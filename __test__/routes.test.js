2bc6cafff6455cfba7e1b7379c6d5fe5057e67dd
Google-arg <bataman911@gmail.com>
2019-11-07 11:25:54 -0300

    comment test

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -1,4 +1,4 @@
-const app = require('../App');
+/* const app = require('../App');
 const supertest = require('supertest');
 const request = supertest(app);
 const {MongoClient} = require('mongodb');
@@ -86,3 +86,4 @@ describe('insert', () => {
     });
 });
  
+ */
